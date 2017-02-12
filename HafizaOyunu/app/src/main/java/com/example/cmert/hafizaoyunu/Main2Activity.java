85ff7fd91f9f4ca0f107647e7b8ea431474d6ea0
mertceyhan <c.mertceyhan@hotmail.com>
2017-02-13 02:32:05 +0300

    Hafıza Oyunu

---
+++ HafizaOyunu/app/src/main/java/com/example/cmert/hafizaoyunu/Main2Activity.java
@@ -0,0 +1,27 @@
+package com.example.cmert.hafizaoyunu;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class Main2Activity extends AppCompatActivity
+{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Intent i = getIntent();
+        String s = i.getStringExtra("mesaj");
+        TextView tv = (TextView) findViewById(R.id.tvIsim);
+        tv.setText(s);
+    }
+}
