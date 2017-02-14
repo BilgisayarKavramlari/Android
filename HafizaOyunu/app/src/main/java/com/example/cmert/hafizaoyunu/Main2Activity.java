95a3845a8ab998df4f4c8ec872308787e3e94015
mertceyhan <c.mertceyhan@hotmail.com>
2017-02-14 11:57:39 +0300

    HafizaOyunu

--- HafizaOyunu/app/src/main/java/com/example/cmert/hafizaoyunu/Main2Activity.java
+++ HafizaOyunu/app/src/main/java/com/example/cmert/hafizaoyunu/Main2Activity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -23,5 +24,12 @@ public class Main2Activity extends AppCompatActivity
         String s = i.getStringExtra("mesaj");
         TextView tv = (TextView) findViewById(R.id.tvIsim);
         tv.setText(s);
+        GridLayout gr = (GridLayout)findViewById(R.id.kartlar);
+        for(int j=0;j<10;j++)
+        {
+            gr.addView(new kart(this));
+        }
+
+
     }
 }
